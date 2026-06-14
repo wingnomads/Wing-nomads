1558ffa55cebcaaed1260670de7fea81d4734c8c
wingnomads <wingnomads@gmail.com>
2026-06-14 17:08:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 alert("SCRIPT ARRANCA");
 
 const SUPABASE_URL = "https://fdpyqsqflwqmyubugitn.supabase.co";
-const SUPABASE_KEY = "TU_PUBLISHABLE_KEY_REAL";
+const SUPABASE_KEY = "sb_publishable_CHWsP3y0fChbq4Pcjq5pvQ_dngXSe69";
 
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
