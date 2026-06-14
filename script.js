cd13af40244b395a1eeb8787cc702d0c85be177b
wingnomads <wingnomads@gmail.com>
2026-06-14 16:23:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://fdpyqsqflwqmyubugitn.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://fdpyqsqflwqmyubugitn.supabase.co";
 const SUPABASE_KEY = "sb_publishable_CHWsP3y0fChbq4Pcjq5pvQ_dngXSe69";
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
